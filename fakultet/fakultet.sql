606a80d7cff3244974a3e509bcbfc5450d4a9289
Pulja1985 <hpuljic85@gmail.com>
2024-05-25 18:50:33 +0200

    Update fakultet.sql

--- fakultet/fakultet.sql
+++ fakultet/fakultet.sql
@@ -1,8 +1,13 @@
-----create database fakultet;
-----use fakultet;
+--create database fakultet;
+--use fakultet;
 --create table radnik(
 --sifra int,
 --ime varchar(50),
 --prezime varchar(50),
---nadredeni int
+--nadredeni varchar(50)
 --);
+insert into radnik values
+(1001, 'hrvoje', 'puljicilic', 'tjakopec')
+(1002, 'dominik', 'puljicilic', 'tjakopec')
+(1003, 'marina', 'trdin', 'tjakopec')
+(1004, 'hrvoje', 'trdin', 'tjakopec')
