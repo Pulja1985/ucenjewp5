56073249614c524138da787aa1b5e4dc0381814a
Pulja1985 <hpuljic85@gmail.com>
2024-05-25 15:46:50 +0200

    racuni

---
+++ racuni.sql
@@ -0,0 +1,16 @@
+--create database racuni;
+--use racuni
+--crate table racun(
+--sifra int,
+--rednibroj int,
+--datum datetime,
+--kupac varchar(50),
+--placeno bit
+--);
+--create table stavke racuna(
+--racun int,
+--artikl varchar(50),
+--cijena decimal(18,2),
+--kolicina int,
+--popust int
+--);
