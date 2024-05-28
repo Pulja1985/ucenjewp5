63c3bd521dc00701bf1f4997658e3c46ef81311c
Pulja1985 <hpuljic85@gmail.com>
2024-05-28 15:01:30 +0200

    Create hrvoje puljić fakulktet2.sql

---
+++ hrvoje puljić fakulktet2.sql
@@ -0,0 +1,17 @@
+--create database fakultet2;
+--use fakultet2;
+--create table fakulteti(
+--ime varchar(50),
+--adresa varchar(50),
+--brojstudenata int
+--);
+--create table studenti(
+--ime varchar(50),
+--prezime varchar(50),
+--godinastudija int
+--);
+--create table kolegiji(
+--nazivkolegija varchar(50),
+--predmet varchar(50),
+--profesor varchar(50)
+--);
