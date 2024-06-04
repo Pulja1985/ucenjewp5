cf023a201fc15ca327d7c56e55fddd0ce1d25e12
Pulja1985 <hpuljic85@gmail.com>
2024-06-04 15:37:22 +0200

    Update doktorska ordinacija.sql

--- doktorska ordinacija/doktorska ordinacija.sql
+++ doktorska ordinacija/doktorska ordinacija.sql
@@ -1,18 +1,23 @@
---create database doktorska_ordinacija;
---use doktorska_ordinacija;
---create table doktori(
---ime varchar(50),
---prezime varchar(50),
---zavseni_smjer varchar(50)
---);
---create table pacijenti(
---ime varchar(50),
---prezime varchar(50),
---vrsta_ljecenja varchar(50)
---);
---create table medicinske_sestre(
---ime varchar(50),
---prezime varchar(50),
---strucna_sprema varchar(50),
---pacijent_za_kojeg_je_zaduzena varchar(50)
---);
+use master;
+go
+drop database if exists doktorska_ordinacija;
+go
+create database doktorska_ordinacija;
+go
+use doktorska_ordinacija;
+create table doktori(
+ime varchar(50),
+prezime varchar(50),
+zavseni_smjer varchar(50)
+);
+create table pacijenti(
+ime varchar(50),
+prezime varchar(50),
+vrsta_ljecenja varchar(50)
+);
+create table medicinske_sestre(
+ime varchar(50),
+prezime varchar(50),
+strucna_sprema varchar(50),
+pacijent_za_kojeg_je_zaduzena varchar(50)
+);
