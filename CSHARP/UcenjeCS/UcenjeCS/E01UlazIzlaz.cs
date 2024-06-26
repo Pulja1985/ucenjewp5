a1f064ab85d6393ec62f843165d2b768a876a0d4
Pulja1985 <hpuljic85@gmail.com>
2024-06-26 19:51:47 +0200

    UcenjeCS

---
+++ CSHARP/UcenjeCS/UcenjeCS/E01UlazIzlaz.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E01UlazIzlaz
+    {
+
+
+        public static void izvedi()
+        {
+
+
+            Console.WriteLine("Hello");
+
+            Console.Write( "Upiši svoje ime");
+
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Unijeli ste" + ime);
+
+            Console.WriteLine( "unijeli ste {0} ",ime);
+
+
+
+            Console.WriteLine( "Unesi adresu");
+
+            string adresa = Console.ReadLine();
+
+
+            Console.WriteLine( "unesi grad");
+
+            string grad=Console.ReadLine(); 
+
+
+
+            Console.WriteLine(adresa);
+            Console.WriteLine(grad);
+
+            Console.WriteLine( "Upiši svoje prezime");
+
+            string prezime = Console.ReadLine();
+
+            Console.WriteLine( "Unešeno" + prezime);
+
+            Console.WriteLine("unešeno {0}", prezime);
+
+
+
+            Console.WriteLine("upiši naziv svog auta");
+
+            string naziv = Console.ReadLine();
+
+            Console.WriteLine("Dodao si ime {0}",naziv);
+
+
+
+            Console.WriteLine("upiši naziv svog psa");
+
+            string psa= Console.ReadLine();
+
+            Console.WriteLine("ime psa je {0}",psa);
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ CSHARP/UcenjeCS/UcenjeCS/E02VarijableTipoviPodataka.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E02VarijableTipoviPodataka
+    {
+
+
+
+        public static void Izvedi()
+        {
+
+            int i;
+
+
+            i=7; 
+
+
+            Console.WriteLine(i);
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
