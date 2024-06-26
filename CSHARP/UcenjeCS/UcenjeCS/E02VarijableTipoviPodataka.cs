1ff00cdad212d4dda5b3c0a9ac021648d99b391d
Pulja1985 <hpuljic85@gmail.com>
2024-06-26 20:51:49 +0200

    Update E02VarijableTipoviPodataka.cs

--- CSHARP/UcenjeCS/UcenjeCS/E02VarijableTipoviPodataka.cs
+++ CSHARP/UcenjeCS/UcenjeCS/E02VarijableTipoviPodataka.cs
@@ -22,6 +22,92 @@ namespace UcenjeCS
 
             Console.WriteLine(i);
 
+            Console.Write("Molim upisati cijeli broj veći od nule: ");
+
+            int broj = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(broj + 1);
+
+            Console.WriteLine(broj + 2);
+
+            Console.Write("Unesi decimalni broj(, je oznaka za decimalni broj): " );
+
+            Console.WriteLine(float.Parse (Console.ReadLine())+1);
+
+
+
+            bool uvijet = false;
+
+            Console.WriteLine(uvijet);
+
+
+            int j = 3;
+
+            Console.WriteLine(j == 3);
+            Console.WriteLine(j!=6);
+
+            i = 0;
+            i = i + 1;
+            i += 1;
+            i++;
+            ++i;
+
+            i = 1;
+            Console.WriteLine(i++);
+            Console.WriteLine(i);
+            Console.WriteLine(++i);
+            Console.WriteLine(i);
+
+            i--;
+            --i;
+
+
+
+            int x = 0, y=1;
+            x = ++x - y;
+            y = x-- + y;
+            Console.WriteLine( x+y);
+
+
+
+
+
+            Console.WriteLine("9%2={0}", 9 % 2);
+            Console.WriteLine("8%2={0}", 8 % 2);
+
+
+            x = 5;
+            y = 2;
+
+            Console.WriteLine(x / (float)y); ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
