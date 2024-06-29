68ea3f1063a147ed55a9736d8512b8fd2e2cbedd
Pulja1985 <hpuljic85@gmail.com>
2024-06-29 17:08:38 +0200

    Update Program.cs

--- CSHARP/Vježbanje Hrvoje/vježbanje1/Program.cs
+++ CSHARP/Vježbanje Hrvoje/vježbanje1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+
+
+
+
+using vježbanje1;
+
+VZ1.Izvedi();
+
+
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
