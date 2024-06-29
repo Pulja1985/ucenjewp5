b896fb4c51fc569303ccf239caef5044f5039406
Pulja1985 <hpuljic85@gmail.com>
2024-06-29 17:07:00 +0200

    Vježbe

---
+++ CSHARP/Vježbanje Hrvoje/vježbanje1/VZ1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vježbanje1
+{
+    internal class VZ1
+    {
+
+        public static void Izvedi()
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
